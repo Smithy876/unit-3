0f54a55f63a2bb5e730eebb0a6f4d82eeeb5dece
Smithy876 <58190181+Smithy876@users.noreply.github.com>
2020-10-31 01:11:38 -0500

    lesson 1 finish

--- js/main.js
+++ js/main.js
@@ -4,11 +4,25 @@ window.onload = function(){
     //SVG dimension variables
     var w = 900, h = 500;
 
-     //Example 1.2 line 1...container block
+    //Example 1.2 line 1...container block
     var container = d3.select("body") //get the <body> element from the DOM
         .append("svg") //put a new svg in the body
         .attr("width", w) //assign the width
         .attr("height", h) //assign the height
         .attr("class", "container") //always assign a class (as the block name) for styling and future selection
         .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+
+    //innerRect block
+    var innerRect = container.append("rect") //new rect in SVG
+        .datum(400) //single value
+        .attr("width", function(d){ //rect width
+            return d * 2;
+        })
+        .attr("height", function(d){ //rect height
+            return d;
+        })
+        .attr("class", "innerRect")
+        .attr("x", 50)
+        .attr("y", 50)
+        .style("fill", "#FFFFFF");
 };
