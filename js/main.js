21172b0c510e3387e6c1d5911fb1c82ba94a45a3
Smithy876 <58190181+Smithy876@users.noreply.github.com>
2020-12-11 19:47:36 -0600

    Chapter 12

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@
     //pseudo-global variables
     var attrArray = ["total-pop", "percent-below-poverty-level", "median-income", "work-outside-place-of-residence", "non-white-hispanic"];
     var expressed = attrArray[0]; //initial attribute
+    var yMax = 250000 //an attempt at creating a variable to change each time a new attribute is selected so the y axis of the chart can update its height
 
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 473,
+        leftPadding = 40,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+    //create a scale to size bars proportionally to frame and for axis
+    var yScale = d3.scaleLinear()
+        .range([463, 0])
+        .domain([0, yMax]);
 
     //begin script when window loads
     window.onload = setMap();
@@ -23,6 +38,7 @@
             .attr("width", width)
             .attr("height", height);
 
+        // variable to add background to map, kept just in case, you never know these days
         // var backing = map.append("rect")
         //     .attr("class", "backing")
         //     .attr("width", width)
@@ -72,8 +88,18 @@
                 .attr("class", "lakes")
                 .attr("d", path);
 
+            var mapTitle = map.append("text")
+                .attr("x", 400)
+                .attr("y", 23)
+                .attr("class", "mapTitle")
+                .text("Distinctions Between the Municipalities of Dane County");
+
+
             //add coordinated visualization to the map
             setChart(csvData, colorScale);
+
+            //create dropdown menu
+            createDropdown(csvData);
         };
     };
 
@@ -81,10 +107,10 @@
     function joinData(daneMunicipalities, csvData){
         //loop through csv to assign each set of csv attribute values to geojson municipality
         for (var i=0; i<csvData.length; i++){
-            var csvMuni = csvData[i]; //the current region
+            var csvMuni = csvData[i]; //the current municipality
             var csvKey = csvMuni.LABEL; //the CSV primary key
 
-            //loop through geojson regions to find correct municipality
+            //loop through geojson municipalities to find correct municipality
             for (var a=0; a<daneMunicipalities.length; a++){
 
                 var geojsonProps = daneMunicipalities[a].properties; //the current municipality geojson properties
@@ -141,6 +167,23 @@
         return colorScale;
     };
 
+    // old attempt at doing a scale updater
+    // function yScale(data){
+    //     var yScale = d3.scaleLinear()
+    //         .range([463, 0])
+    //         .domain([0, Math.max(data[0])]);
+    //
+    //     //create vertical axis generator
+    //     var yAxis = d3.axisLeft()
+    //         .scale(yScale);
+    //
+    //     //place axis
+    //     var axis = chart.append("g")
+    //         .attr("class", "axis")
+    //         .attr("transform", translate)
+    //         .call(yAxis);
+    // };
+
     function setEnumerationUnits(daneMunicipalities, map, path, colorScale){
         //add Dane County municipalities to map
         var municipalities = map.selectAll(".municipalities")
@@ -158,20 +201,21 @@
                 } else {
                     return "#535353";
                 }
-            });
+            })
+            .on("mouseover", function(event, d){
+                highlight(d.properties);
+            })
+            .on("mouseout", function(event, d){
+                dehighlight(d.properties);
+            })
+            .on("mousemove", moveLabel);
+
+        var desc = municipalities.append("desc")
+            .text('{"stroke": "#000", "stroke-width": "0.5px"}');
     };
 
     //function to create coordinated bar chart
     function setChart(csvData, colorScale){
-        //chart frame dimensions
-        var chartWidth = window.innerWidth * 0.425,
-            chartHeight = 473,
-            leftPadding = 25,
-            rightPadding = 2,
-            topBottomPadding = 5,
-            chartInnerWidth = chartWidth - leftPadding - rightPadding,
-            chartInnerHeight = chartHeight - topBottomPadding * 2,
-            translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
 
         //create a second svg element to hold the bar chart
         var chart = d3.select("body")
@@ -187,11 +231,10 @@
             .attr("height", chartInnerHeight)
             .attr("transform", translate);
 
-
-        //create a scale to size bars proportionally to frame
+        // create a scale to size bars proportionally to frame
         var yScale = d3.scaleLinear()
-            .range([463, 0])
-            .domain([0, 250000]);
+            .range([450, 0])
+            .domain([0, yMax]);
 
         //create vertical axis generator
         var yAxis = d3.axisLeft()
@@ -222,28 +265,230 @@
                 return "bar " + d.LABEL;
             })
             .attr("width", chartInnerWidth / csvData.length - 1)
-            .attr("x", function(d, i){
-                return i * (chartInnerWidth / csvData.length) + leftPadding;
+            .on("mouseover", function(event, d){
+                highlight(d);
             })
+            .on("mouseover", function(event, d){
+                dehighlight(d);
+            })
+            .on("mousemove", moveLabel);
+
+            //set bar positions, heights, and colors
+            updateChart(bars, csvData.length, colorScale);
+
+        //create chart title
+        var chartTitle = chart.append("text")
+            .attr("x", 80)
+            .attr("y", 40)
+            .attr("class", "chartTitle")
+            .text("Total Population");
+
+        var desc = bars.append("desc")
+            .text('{"stroke": "none", "stroke-width": "0px"}');
+
+    };
+
+    //function to position, size, and color bars in chart
+    function updateChart(bars, n, colorScale, attrArray){
+        //position bars
+        bars.attr("x", function(d, i){
+                return i * (chartInnerWidth / n) + leftPadding;
+            })
+            //size/resize bars
             .attr("height", function(d, i){
                 return 463 - yScale(parseFloat(d[expressed]));
             })
             .attr("y", function(d, i){
                 return yScale(parseFloat(d[expressed])) + topBottomPadding;
             })
+            //color/recolor bars
             .style("fill", function(d){
-                return colorScale(d[expressed]);
+                var value = d[expressed];
+                if(value) {
+                    return colorScale(value);
+                } else {
+                    return "#ccc";
+                }
             });
 
-        var chartTitle = chart.append("text")
-            .attr("x", 40)
-            .attr("y", 40)
-            .attr("class", "chartTitle")
-            .text("Population of Municipalities in Dane County");
+        //change titles, failed yMax experiment
+        var chartTitle = d3.select(".chartTitle")
+            if (expressed[0] == "t") {
+                chartTitle.text("Total Population")
+                // yMax == 250000;
+            } else if (expressed[0] == "p") {
+                chartTitle.text("Percent of Residents under the Poverty Level")
+                // yMax == 50;
+            } else if (expressed[0] == "m") {
+                chartTitle.text("Median Income")
+                // yMax == 150000;
+            } else if (expressed[0] == "w") {
+                chartTitle.text("Percent of Employed Residents Working in a Different Municipality")
+                // yMax == 100;
+            } else {
+                chartTitle.text("Percent of Non-White and Hispanic Residents")
+                // yMax == 50;
+            };
 
-
+        // change yMax (max height of y axis) depending on variable
+        // if (attrArray == 0) {
+        //     yMax == 250000;
+        // } else if (attrArray == 1) {
+        //     yMax == 50;
+        // } else if (attrArray == 2) {
+        //     yMax == 150000;
+        // } else if (attrArray == 3) {
+        //     yMax == 100;
+        // } else {
+        //     yMax == 50;
+        // };
     };
 
 
+    // ON USER SELECTION:
+    // Step 1. Change the expressed attribute
+    // Step 2. Recreate the color scale with new class breaks
+    // Step 3. Recolor each enumeration unit on the map
+    // Step 4. Re-sort each bar on the bar chart
+    // Step 5. Resize each bar on the bar chart
+    // Step 6. Recolor each bar on the bar chart
+    // Step 7. Recalculate scale on the bar chart
+
+    //function to create a dropdown menu for attribute selection
+    function createDropdown(csvData){
+        //add select element
+        var dropdown = d3.select("body")
+            .append("select")
+            .attr("class", "dropdown")
+            .on("change", function(){
+                changeAttribute(this.value, csvData)
+            });
+
+        //add initial option
+        var titleOption = dropdown.append("option")
+            .attr("class", "titleOption")
+            .attr("disabled", "true")
+            .text("Select Attribute");
+
+        //add attribute name options
+        var attrOptions = dropdown.selectAll("attrOptions")
+            .data(attrArray)
+            .enter()
+            .append("option")
+            .attr("value", function(d){ return d })
+            .text(function(d){ return d });
+    };
+
+    //dropdown change listener handler
+    function changeAttribute(attribute, csvData){
+        //change the expressed attribute
+        expressed = attribute;
+
+        //recreate the color scale
+        var colorScale = makeColorScale(csvData);
+
+        //animate map
+        var municipalities = d3.selectAll(".municipalities")
+            .transition()
+            .duration(1000)
+            .style("fill", function(d){
+                var value = d.properties[expressed];
+                if(value) {
+                    return colorScale(value);
+                } else {
+                    return "#535353";
+                }
+            });
+
+        //resort and animate bar ch
+        var bars = d3.selectAll(".bar")
+            //re-sort bars
+            .sort(function(a, b){
+                return b[expressed] - a[expressed];
+            })
+            .transition() //add animation
+            .delay(function(d, i){
+                return i * 20
+            })
+            .duration(500);
+
+        updateChart(bars, csvData.length, colorScale);
+    };
+
+    //function to highlight enumeration units and bars
+    function highlight(props){
+        //change stroke
+        var selected = d3.selectAll("." + props.LABEL)
+            .style("stroke", "blue")
+            .style("stroke-width", "2");
+        var desc = municipalities.append("desc")
+            .text('{"stroke": "#000", "stroke-width": "0.5px"}');
+    };
+
+    //function to reset the element style on mouseout
+    function dehighlight(props){
+        var selected = d3.selectAll("." + props.LABEL)
+            .style("stroke", function(){
+                return getStyle(this, "stroke")
+            })
+            .style("stroke-width", function(){
+                return getStyle(this, "stroke-width")
+            });
+
+        function getStyle(element, styleName){
+            var styleText = d3.select(element)
+                .select("desc")
+                .text();
+
+            var styleObject = JSON.parse(styleText);
+
+            return styleObject[styleName];
+        };
+
+        d3.select(".infolabel")
+            .remove();
+    };
+
+    //function to create dynamic label
+    function setLabel(props){
+        //label content
+        var labelAttribute = "<h1>" + props[expressed] +
+            "</h1><b>" + expressed + "</b>";
+
+        //create info label div
+        var infolabel = d3.select("body")
+            .append("div")
+            .attr("class", "infolabel")
+            .attr("id", props.LABEL + "_label")
+            .html(labelAttribute);
+
+        var Name = infolabel.append("div")
+            .attr("class", "labelname")
+            .html(props.name);
+    };
+
+    //function to move info label with mouse
+    function moveLabel(){
+        //get width of label
+        var labelWidth = d3.select(".infolabel")
+            .node()
+            .getBoundingClientRect()
+            .width;
+
+        //use coordinates of mousemove event to set label coordinates
+        var x1 = event.clientX + 10,
+            y1 = event.clientY - 75,
+            x2 = event.clientX - labelWidth - 10,
+            y2 = event.clientY + 25;
+
+        //horizontal label coordinate, testing for overflow
+        var x = event.clientX > window.innerWidth - labelWidth - 20 ? x2 : x1;
+        //vertical label coordinate, testing for overflow
+        var y = event.clientY < 75 ? y2 : y1;
+
+        d3.select(".infolabel")
+            .style("left", x + "px")
+            .style("top", y + "px");
+    };
 
 })();
